855cd7c082bf2200bea8bb010d80f5ed55d219d7
tomoya8 <gea01051@yahoo.co.jp>
2016-04-08 00:15:00 +0900

    added font-anzumoji

---
+++ Casks/font-anzumoji.rb
@@ -0,0 +1,11 @@
+cask 'font-anzumoji' do
+  version '2012.5'
+  sha256 '8e53ea565d6987d638dfb3d7994594f39ccf2e6bae9d745bf3ca44d72a42fa36'
+
+  url 'http://www8.plala.or.jp/p_dolce/ap2/apjfont.zip'
+  name 'あんずもじ'
+  homepage 'http://www8.plala.or.jp/p_dolce/'
+  license :oss
+
+  font 'APJapanesefont.ttf'
+end
