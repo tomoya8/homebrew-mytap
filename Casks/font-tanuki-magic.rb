dc24687a4a8e4333a04272f2e75839320b6a2d30
tomoya8 <kon@sogalabo.jp>
2016-08-28 14:56:06 +0900

    Modified to use UTF-8

--- Casks/font-tanuki-magic.rb
+++ Casks/font-tanuki-magic.rb
@@ -3,7 +3,7 @@ cask 'font-tanuki-magic' do
   sha256 '659a4a124d5864680152f77f52e892d8abc3a55ba695a265204f1dcc67c585a7'
 
   url 'http://tanukifont.com/download/TanukiMagic_1_12.zip'
-  name '$B$?$L$-L}@-%^%8%C%/(B'
+  name 'たぬき油性マジック'
   homepage 'http://tanukifont.com/tanuki-permanent-marker/'
   license :oss
 
