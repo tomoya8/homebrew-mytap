c7d749ca87c24c3ae5851e11f398218d8c96b64d
tomoya8 <gea01051@yahoo.co.jp>
2017-03-16 16:52:58 +0900

    Added cinderella, hype3, scilab6.

---
+++ Casks/cinderella.rb
@@ -0,0 +1,10 @@
+cask 'cinderella' do
+  version '2.9'
+  sha256 '46a8fedd655e902adbf4ce08fe2e8d1e7fbecced0afd2333df02a942a20c2747' 
+
+  url 'https://www.cinderella.de/files/release/Cinderella2.zip'
+  name 'Cinderella'
+  homepage 'https://www.cinderella.de/tiki-index.php'
+
+  app 'Cinderella2.app'
+end
