9c10552a1fd92a51fdb3ed89592a96d86a8345b5
Tomoya Konishi <kon@sogalabo.jp>
2018-01-05 22:12:57 +0900

    Added TownWifi.

---
+++ Casks/townwifi.rb
@@ -0,0 +1,10 @@
+cask 'townwifi' do
+  version '1.0.1'
+  sha256 '7e493488f4139645d5060f52651a59a906e46b12efe8a2993a0c07e2e505435b'
+
+  url 'https://storage.googleapis.com/townwifi-downloads/mac/installer/TownWifi.pkg'
+  name 'TownWifi'
+  homepage 'http://townwifi.jp/'
+
+  app 'TownWifi.app'
+end
