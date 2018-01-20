64b8dcdbb96617f389da343f84e1d58c076983f2
こいぬ <kon@sogalabo.jp>
2018-01-20 11:32:17 +0900

    Updated 1piece.

--- Casks/1piece.rb
+++ Casks/1piece.rb
@@ -1,5 +1,5 @@
 cask '1piece' do
-  version '0.18.0'
+  version '0.19.0'
   sha256 'd8123550484267a34c18ba671ed6743fb70cc8e337b1b547cb26b66bddb1c23c'
 
   url 'http://www001.upp.so-net.ne.jp/app1piece/1Piece-0.19.0.zip'
