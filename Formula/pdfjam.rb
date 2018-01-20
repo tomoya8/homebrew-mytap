528417f6d718b319187e574dee1418a07a92773a
こいぬ <kon@sogalabo.jp>
2018-01-20 11:30:30 +0900

    Updated pdfjam.

--- Formula/pdfjam.rb
+++ Formula/pdfjam.rb
@@ -5,7 +5,7 @@ class Pdfjam < Formula
   version "2.08"
   sha256 "c731c598cfad076c985526ff89cbf34423a216101aa5e2d753a71de119ecc0f3"
 
-  depends_on :tex
+  #depends_on :tex
 
   def install
     bin.install Dir["bin/*"]
