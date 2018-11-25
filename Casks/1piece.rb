22eaeeb470f595370c0cb1a62bf73f79bd368754
こいぬ <kon@sogalabo.jp>
2018-11-25 12:41:00 +0900

    Updated 1piece 1.2.

--- Casks/1piece.rb
+++ Casks/1piece.rb
@@ -1,12 +1,12 @@
 cask '1piece' do
-  version '0.19.0'
-  sha256 'd8123550484267a34c18ba671ed6743fb70cc8e337b1b547cb26b66bddb1c23c'
+  version '1.2'
+  sha256 '2c70832ae15b881cc2f4178b45b30e1af611015a70e59fff202db22bbdfe427a'
 
-  url 'http://www001.upp.so-net.ne.jp/app1piece/1Piece-0.19.0.zip'
+  url 'https://app1piece.com/1Piece-1.2.zip'
   name '1Piece'
   homepage 'http://www001.upp.so-net.ne.jp/app1piece/'
 
-  accessibility_access true
+  #accessibility_access true
 
   app '1Piece.app'
 
