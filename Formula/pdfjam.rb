63cc297bed2d2aa21402c8001aa70a8a6c0b1e6a
Tomoya Konishi <gea01051@yahoo.co.jp>
2026-05-28 10:23:12 +0900

    Add pdfjam

---
+++ Formula/pdfjam.rb
@@ -0,0 +1,17 @@
+class Pdfjam < Formula
+  desc "The pdfjam package for manipulating PDF files"
+  homepage "https://github.com/pdfjam/pdfjam"
+  url "https://github.com/pdfjam/pdfjam/releases/download/v4.3.1/pdfjam-4.3.1.tar.gz"
+  sha256 "4423745a9708335afdbc68fb27b9eec3d177ccbb755fb74e37ecc0c77e88fd09"
+  license "GPL-2.0"
+
+  def install
+    bin.install Dir["bin/*"]
+    man.install "man"
+    zsh_completion.install "completions/zsh/_pdfjam"
+  end
+
+  test do
+    system "false"
+  end
+end
