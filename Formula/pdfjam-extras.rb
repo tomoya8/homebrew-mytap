b3cda5aae0c7199057e161c3075f3819bbc42e99
Tomoya Konishi <konishi@anan-nct.ac.jp>
2026-05-28 10:03:41 +0900

    Add files via upload
    Add pdfjam-extras

---
+++ Formula/pdfjam-extras.rb
@@ -0,0 +1,18 @@
+class PdfjamExtras < Formula
+  desc "Some unsupported 'wrapper' scripts for pdfjam"
+  homepage "https://github.com/tomoya8/pdfjam-extras"
+  url "https://github.com/tomoya8/pdfjam-extras/archive/refs/tags/v2.09.zip"
+  sha256 "4f5079483460046de1d2606c77a77aa620956230b58dbd647e0184f54ddb88ef"
+  license "GPL-2.0"
+
+  depends_on :pdfjam
+
+  def install
+    bin.install Dir["bin/*"]
+    man.install "man1"
+  end
+
+  test do
+    system "false"
+  end
+end
